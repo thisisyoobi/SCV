74e208ffa30748786740eecf5949d5da55a997b6
GeonminJun <disklogrrr0914@gmail.com>
2020-12-15 23:02:22 +0900

    U_15 check SNMP Service

---
+++ U_15.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <fcntl.h>
+#define BUF_SIZE 1024
+
+/* 점검기준 : world writable 파일 점검 
+			  world writable file 존재하면 취약, 없으면 양호 */
+
+void U_15() {
+	FILE* fp;
+	int n1;
+	char count1[BUF_SIZE];
+	char setting[BUF_SIZE] = "0";
+	int ret1;
+
+	system("find /dev -type f -exec ls -l {} \\; | wc -l > output15.txt");
+
+	if ((fp = fopen("output15.txt", "r")) == NULL) {
+		printf("[U-15] world writable 파일 점검(상) : 점검 오류\n");
+		return;
+	}
+	fgets(count1, sizeof(count1), fp);
+	ret1 = strcmp(count1, setting);
+	if (ret1 == 10)
+		printf("[U-15] world writable 파일 점검(상) : 양호\n");
+	else
+		printf("[U-15] world writable 파일 점검(상) : 취약\n");
+
+	system("rm -f output15.txt");
+}
