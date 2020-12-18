f1b3a6e628d8fd0ebe71f1737530e3c9087c0956
e70e6916 <dgkim6916@naver.com>
2020-12-18 23:09:37 +0900

    U_06 check file and directory owner settings

---
+++ U_06.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void U_06()
+{
+        FILE *nouser_fp, *nogroup_fp;
+	char buf[100];
+
+        if( (nouser_fp = popen( "echo find / -nouser -print", "r" )) == NULL || (nogroup_fp = popen( "echo find / -nogroup -print", "r" )) == NULL ) {
+                printf("[U-06] 파일 및 디렉터리 소유자 설정 (상) : 점검 오류\n");
+                return;
+        }
+		
+	if( fgets(buf, sizeof(buf), nouser_fp) != NULL || fgets(buf, sizeof(buf), nogroup_fp) != NULL )
+		printf("[U-06] 파일 및 디렉터리 소유자 설정 (상) : 취약\n");
+	else
+		printf("[U-06] 파일 및 디렉터리 소유자 설정 (상) : 양호\n");
+
+	
+	pclose(nouser_fp);
+	pclose(nogroup_fp);	
+}
+
