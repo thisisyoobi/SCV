b05afb02beab46a1a8e8c30a8ec38ba9c1f4327a
woosuk1 <74864092+woosuk1@users.noreply.github.com>
2020-12-18 15:52:46 +0900

    U_55 check hosts.lpd file owner and authority

--- U_55.c
+++ U_55.c
@@ -5,6 +5,7 @@
 
 void U_55()
 {
+	FILE *fp; //변수 선언
 	struct stat buf;
 	if((fp =fopen("/etc/hosts.lpd", "r")) == NULL){
 		printf("[U-55] hosts.lpd 파일 소유자 및 권한 설정(하): 점검 오류\n");
@@ -14,10 +15,10 @@ void U_55()
 	stat("/etc/hosts.lpd", &buf);
 	if(buf.st_uid == 0){
 		if(buf.st_mode &S_IRUSR && buf.st_mode &S_IWUSR){
-			printf("[U-55] hosts.lpd 파일 소유자 및 권한 설정(하): 파일 소유자 및 권한 설정 (상) : 양호\n");
+			printf("[U-55] hosts.lpd 파일 소유자 및 권한 설정(하) : 양호\n");
 		}
 		else
-			printf("[U-55] hosts.lpd 파일 소유자 및 권한 설정(하): : 취약\n");
+			printf("[U-55] hosts.lpd 파일 소유자 및 권한 설정(하): 취약\n");
 		
 	}
 	else
