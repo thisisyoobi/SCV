f5a5ac17a8395978edf8ae20f5d4d57fdd9ce582
woosuk1 <giuseog@gmail.com>
2020-12-18 15:41:07 +0900

    U_54 check Session Timeout

---
+++ U_54.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define LINE 512
+#define SESSION_TIMEOUT "TMOUT"
+/*점검기준 : Session Timeout 설정 */
+void U_54()
+{
+	FILE *fp;
+	char buf[LINE];
+	
+	if((fp =fopen("/etc/profile", "r")) == NULL){
+		printf("[U-54]Session Timeout 설정(하) : 점검 오류\n");
+		return;
+	}
+	
+	while(fgets(buf, sizeof(buf), fp)){
+		if(strncmp(SESSION_TIMEOUT, buf, 5) ==0){
+			printf("[U-54] Session Timeout 설정 (중) : 양호\n");
+			fclose(fp);
+			exit(0);
+		
+		}
+	}
+	printf("[U-54] Session Timeout 설정 (중) : 취약\n");
+}
+
+
