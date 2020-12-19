1c95831179632d6afad3dc03bda74d97a4b6f2a1
woosuk1 <74864092+woosuk1@users.noreply.github.com>
2020-12-19 16:48:42 +0900

    Update U_66.c

--- src/U_66.c
+++ src/U_66.c
@@ -3,25 +3,26 @@
 #include <sys/types.h>
 /* at 파일 소유자 및 권한 설정 */
 
-void U_66()
+int U_66()
 {
 	FILE *fp;
 	struct stat buf;
 	if((fp =fopen("/etc/cron.d/at.allow", "r")) == NULL){
-		printf("[U-66] /etc/cron.d/at.allow 파일 소유자 및 권한 설정(중): 점검 오류\n");
-		return;
+		printf("[U-66] at 파일 소유자 및 권한 설정(중): 점검 오류(파일 탐색 불가)\n");
+		return 0;
 	}
 	
 	stat("/etc/cron.d/at.allow", &buf);
 	if(buf.st_uid == 0){
 		if(buf.st_mode &S_IRUSR && buf.st_mode &S_IWUSR && buf.st_mode &S_IRGRP){
-			printf("[U-66] /etc/cron.d/at.allow 파일 소유자 및 권한 설정 (중) : 양호\n");
+			printf("[U-66] at 파일 소유자 및 권한 설정 (중) : 양호\n");
+			return 1;
 		}
 		else
-			printf("[U-66] /etc/cron.d/at.allow 파일 소유자 및 권한 설정 (중) : 취약\n");
-		
+			printf("[U-66] at 파일 소유자 및 권한 설정 (중) : 취약\n");
+			return 2;
 	}
 	else
-		printf("[U-66] /etc/cron.d/at.allow 파일 소유자 및 권한 설정 (중) : 취약\n");
-		
+		printf("[U-66] at 파일 소유자 및 권한 설정 (중) : 취약\n");
+		return 2;
 }
