0c70ebf418f20b75e6a9031e7b51b61f2da7d011
thisisyoobi <75841024+thisisyoobi@users.noreply.github.com>
2020-12-19 16:41:24 +0900

    Update U_68.c

--- src/U_68.c
+++ src/U_68.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void U_68()
+int U_68()
 {
    char buf[512];
    printf("[U-68] SNMP 서비스 Community String의 복잡성 설정 (중) : 수동 확인\n");
-   return;
+   return 3;
 }
