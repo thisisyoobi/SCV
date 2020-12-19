861817d45372e2a61501af1bdede7422d82b4e13
woosuk1 <74864092+woosuk1@users.noreply.github.com>
2020-12-19 16:20:09 +0900

    Update U_11.c

--- src/U_11.c
+++ src/U_11.c
@@ -3,25 +3,26 @@
 #include <sys/types.h>
 /* /etc/syslog.conf 파일 소유자 및 권한 설정 */
 // commit change1
-void U_11()
+int U_11()
 {
 	FILE *fp;
 	struct stat buf;
 	
 	if((fp =fopen("/etc/syslog.conf", "r")) == NULL){
-		printf("[U-11] /etc/syslog.conf 파일 소유자 및 권한 설정(상): 점검 오류\n");
-		return;
+		printf("[U-11] /etc/syslog.conf 파일 소유자 및 권한 설정(상): 점검 오류(파일 탐색 불가)\n");
+		return 0;
 	}
 	stat("/etc/syslog.conf", &buf);
 	if(buf.st_uid == 0){
 		if(buf.st_mode &S_IRUSR && buf.st_mode &S_IWUSR && buf.st_mode &S_IRGRP && buf.st_mode &S_IROTH){
 			printf("[U-11] /etc/syslog.conf 파일 소유자 및 권한 설정 (상) : 양호\n");
+			return 1;
 		}
 		else
 			printf("[U-11] /etc/syslog.conf 파일 소유자 및 권한 설정 (상) : 취약\n");
-		
+			return 2;
 	}
 	else
 		printf("[U-11] /etc/syslog.conf 파일 소유자 및 권한 설정 (상) : 취약\n");
-		
+		return 2;
 }
