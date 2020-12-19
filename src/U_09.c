176b0164681284b851a3d0570c4dfa7ddddc8319
woosuk1 <74864092+woosuk1@users.noreply.github.com>
2020-12-19 16:18:26 +0900

    Update U_09.c

--- src/U_09.c
+++ src/U_09.c
@@ -3,25 +3,26 @@
 #include <sys/types.h>
 /* /etc/hosts 파일 소유자 및 권한 설정 */
 
-void U_09()
+int U_09()
 {
 	FILE *fp;
 	struct stat buf;
 	if((fp =fopen("/etc/hosts", "r")) == NULL){
-		printf("[U-09] /etc/host 파일 소유자 및 권한 설정 (상) : 점검 오류\n");
-		return;
+		printf("[U-09] /etc/host 파일 소유자 및 권한 설정 (상) : 점검 오류(파일 탐색 불가)\n");
+		return 0;
 	}
 	
 	stat("/etc/hosts", &buf);
 	if(buf.st_uid == 0){
 		if(buf.st_mode &S_IRUSR && buf.st_mode &S_IWUSR){
 			printf("[U-09] /etc/host 파일 소유자 및 권한 설정 (상) : 양호\n");
+			return 1;
 		}
 		else
 			printf("[U-09] /etc/host 파일 소유자 및 권한 설정 (상) : 취약\n");
-		
+			return 2;
 	}
 	else
 		printf("[U-09] /etc/host 파일 소유자 및 권한 설정 (상) : 취약\n");
-		
+		return 2;
 }
