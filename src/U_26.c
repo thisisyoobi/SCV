72cb4f824e6c57a061aba2275535123cc22bc7ab
disklogrrr <73327002+disklogrrr@users.noreply.github.com>
2020-12-19 16:42:06 +0900

    Update U_26.c

--- src/U_26.c
+++ src/U_26.c
@@ -9,7 +9,7 @@
 /* 점검기준 : automountd 서비스 데몬의 실행 여부 점검 
             automountd 서비스 활성화시 취약, 비활성화시 양호 */
 
-void U_26() {
+int U_26() {
    FILE* fp;
    int n1;
    char count1[BUF_SIZE];
@@ -19,15 +19,20 @@ void U_26() {
    system("ps -ef | grep automount | wc -l > output26.txt");
 
    if ((fp = fopen("output26.txt", "r")) == NULL) {
-      printf("[U-26] automountd 제거(상) : 점검 오류\n");
-      return;
+      printf("[U-26] automountd 제거(상) : 점검 오류 (파일 탐색 불가) \n");
+      system("rm -f output26.txt");
+      return 0;
    }
    fgets(count1, sizeof(count1), fp);
    ret1 = strcmp(count1, setting);
-   if (ret1 == 10)
-      printf("[U-26] automountd 제거(상) : 양호\n");
-   else
-      printf("[U-26] automountd 제거(상) : 취약\n");
-
    system("rm -f output26.txt");
+   if (ret1 == 10){
+      printf("[U-26] automountd 제거(상) : 양호\n");
+      return 1;
+   }
+   else{
+      printf("[U-26] automountd 제거(상) : 취약\n");
+      return 2;
+   }
+
 }
