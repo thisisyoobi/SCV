c9a2b0e04bb21b4e41649b5e02a60807ceb9e8ca
woosuk1 <74864092+woosuk1@users.noreply.github.com>
2020-12-19 16:26:46 +0900

    Update U_55.c

--- src/U_55.c
+++ src/U_55.c
@@ -8,20 +8,21 @@ void U_55()
 	FILE *fp; //변수 선언
 	struct stat buf;
 	if((fp =fopen("/etc/hosts.lpd", "r")) == NULL){
-		printf("[U-55] hosts.lpd 파일 소유자 및 권한 설정(하): 점검 오류\n");
-		return;
+		printf("[U-55] hosts.lpd 파일 소유자 및 권한 설정(하): 점검 오류(파일 탐색 불가)\n");
+		return 0;
 	}
 	
 	stat("/etc/hosts.lpd", &buf);
 	if(buf.st_uid == 0){
 		if(buf.st_mode &S_IRUSR && buf.st_mode &S_IWUSR){
 			printf("[U-55] hosts.lpd 파일 소유자 및 권한 설정(하) : 양호\n");
+			return 1;
 		}
 		else
 			printf("[U-55] hosts.lpd 파일 소유자 및 권한 설정(하): 취약\n");
-		
+			return 2;
 	}
 	else
 		printf("[U-55] hosts.lpd 파일 소유자 및 권한 설정(하): : 취약\n");
-		
+		return 2;
 }
