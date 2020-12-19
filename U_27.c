0d11f2b54cd6c847e2454bb113dfd21daf6568d3
GeonminJun <disklogrrr0914@gmail.com>
2020-12-19 14:24:27 +0900

    U_27 Check RPC Service

---
+++ U_27.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+void U_27()
+{
+	char buf[512];
+	FILE* fp;
+	printf("[U-27] RPC 서비스 확인 (상) : 수동 점검\n");
+	fclose(fp);
+	return;
+
+}
