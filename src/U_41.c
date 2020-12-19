673524aa968ecb33848fd84ba06eb32bebe90e1d
disklogrrr <73327002+disklogrrr@users.noreply.github.com>
2020-12-19 17:48:34 +0900

    Update U_41.c

--- src/U_41.c
+++ src/U_41.c
@@ -11,7 +11,7 @@ int U_41()
 	FILE* fp;
 
 	if ((fp = fopen("/etc/apache2/sites-available/000-default.conf", "r")) == NULL) {
-		printf("[U-41] Apache 웹 서비스 영역의 분리 : 점검 오류\n");
+		printf("[U-41] Apache 웹 서비스 영역의 분리 (상) : 점검 오류\n");
 		return 0;
 	}
 	
@@ -63,12 +63,12 @@ int U_41()
 
 	if(result)
 	{
-		printf("[U-41] Apache 웹 서비스 영역의 분리 : 양호\n");
+		printf("[U-41] Apache 웹 서비스 영역의 분리 (상) : 양호\n");
 		return 1;
 	}
 	else
 	{
-		printf("[U-41] Apache 웹 서비스 영역의 분리 : 취약\n");
+		printf("[U-41] Apache 웹 서비스 영역의 분리 (상) : 취약\n");
 		return 2;
 	}
 
