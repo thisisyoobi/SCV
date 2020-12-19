da5090a7b42b7993dc2c7f7e24739e84e1018cf1
thisisyoobi <thisisyoobi@gmail.com>
2020-12-19 05:00:22 +0000

    U_72 Apache hide info

---
+++ U_72.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+void U_72()
+{
+	int result1 = 0;
+	int result2 = 0;
+	char buf[512];
+	FILE* fp;
+
+	if ((fp = fopen("/etc/apache2/apache2.conf", "r")) == NULL) {
+		printf("[U-72] Apache 웹 서비스 정보 숨김 : 점검 오류\n");
+	}
+	
+	const int max = 9999;
+	char line[max];
+	char *pLine;
+	char *p;
+	while(!feof(fp))
+	{
+		pLine = fgets(line, max, fp);
+		if(pLine)
+		{
+			if(strstr(pLine, "ServerTokens Prod"))
+			{
+				if(strchr(pLine, '#'))
+				{
+					result1 = 0;
+				}
+				result1 = 1;
+			}
+			
+			if(strstr(pLine, "ServerSignature Off"))
+			{
+				if(strchr(pLine, '#'))
+				{
+					result2 = 0;
+				}
+				result2 = 1;
+			}
+
+		}
+	}
+	fclose(fp);
+
+	if(result1 && result2)
+		printf("[U-72] Apache 웹 서비스 정보 숨김 : 양호\n");
+	else
+		printf("[U-72] Apache 웹 서비스 정보 숨김 : 취약\n");
+
+}
