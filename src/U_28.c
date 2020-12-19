c76c7854b248c2c90eb753a07bcb6369a3cdb825
disklogrrr <73327002+disklogrrr@users.noreply.github.com>
2020-12-19 16:48:55 +0900

    Update U_28.c

--- src/U_28.c
+++ src/U_28.c
@@ -9,7 +9,7 @@
 /* 점검기준 : NIS 서비스가 활성화 되어있는지 되어있지 않는지 점검 
 				NIS 서비스 활성화시 취약, 비활성화시 양호.*/
 
-void U_28() {
+int U_28() {
 	FILE* fp;
 	int n1;
 	char count1[BUF_SIZE];
@@ -19,18 +19,21 @@ void U_28() {
 	system("ps -ef | egrep \"ypserv|ypbind|ypxfrd|rpc.yppasswdd|rpc.ypupdated\" | wc -l > output28.txt");
 
 	if ((fp = fopen("output28.txt", "r")) == NULL) {
-		printf("[U-28] NIS, NIS+ 점검(상) : 점검 오류\n");
-		return;
+		printf("[U-28] NIS, NIS+ 점검(상) : 점검 오류 (파일 탐색 불가)\n");
+		system("rm -f output28.txt");
+		return 0;
 	}
 
 	fgets(count1, sizeof(count1), fp);
 	ret1 = strcmp(count1, setting);
-
-	if (ret1 == 10)
-		printf("[U-28] NIS, NIS+ 점검(상) : 양호\n");
-	else
-		printf("[U-28] NIS, NIS+ 점검(상) : 취약\n");
-
 	system("rm -f output28.txt");
+	if (ret1 == 10){
+		printf("[U-28] NIS, NIS+ 점검(상) : 양호\n");
+		return 1;
+	}
+	else{
+		printf("[U-28] NIS, NIS+ 점검(상) : 취약\n");
+		return 2;
+	}
 
 }
