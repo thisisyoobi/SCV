9f74943a3dbe121ed7db47332896c7be55dd1d91
disklogrrr <73327002+disklogrrr@users.noreply.github.com>
2020-12-19 16:51:08 +0900

    Update U_57.c

--- src/U_57.c
+++ src/U_57.c
@@ -9,7 +9,7 @@
 /* 점검기준 : 시스템 UMASK 값이 022 이상인지 아닌지 점검 
 				UMASK 값이 022 이상이면 양호, 미만이만 취약. */
 
-void U_57() {
+int U_57() {
 	FILE* fp;
 	char arr[BUF_SIZE];
 	char setting[BUF_SIZE] = "UMASK=022";
@@ -17,15 +17,19 @@ void U_57() {
 
 	system("grep -r 'UMASK=022' /etc/profile > output57.txt");
 	if ((fp = fopen("output57.txt", "r")) == NULL) {
-		printf("[U-57] UMASK 설정 관리(중) : 점검 오류\n");
+		printf("[U-57] UMASK 설정 관리(중) : 점검 오류 (파일 탐색 불가)\n");
+		system("rm -f output57.txt");
 		return;
 	}
 	fgets(arr, sizeof(arr), fp);
 	ret = strcmp(arr, setting);
-	if (ret > 0)
-		printf("[U-57] UMASK 설정 관리(중) : 양호\n");
-	else
-		printf("[U-57] UMASK 설정 관리(중) : 취약\n");
-
 	system("rm -f output57.txt");
+	if (ret > 0){
+		printf("[U-57] UMASK 설정 관리(중) : 양호\n");
+		return 1;
+	}
+	else{
+		printf("[U-57] UMASK 설정 관리(중) : 취약\n");
+		return 2;
+	}
 }
