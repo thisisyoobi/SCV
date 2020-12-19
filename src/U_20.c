7aecb5a9a540d12a39cf9b712120e5ad687deb57
disklogrrr <73327002+disklogrrr@users.noreply.github.com>
2020-12-19 16:22:10 +0900

    Update U_20.c

--- src/U_20.c
+++ src/U_20.c
@@ -9,7 +9,7 @@
 /* 점검기준 : Anonymous FTP 비활성화 점검 
           "passwd" 파일에서 ftp계정이 존재하면 취약, 없으면 양호 */
 
-void U_20() {
+int U_20() {
    FILE* fp;
    int n1;
    char count1[BUF_SIZE];
@@ -19,15 +19,19 @@ void U_20() {
    system("cat /etc/passwd | grep \"ftp\" | wc -l > output20.txt");
 
    if ((fp = fopen("output20.txt", "r")) == NULL) {
-      printf("[U-20] Anonymous FTP 비활성화(상) : 점검 오류\n");
-      return;
+      printf("[U-20] Anonymous FTP 비활성화(상) : 점검 오류 (파일 탐색 불가)\n");
+      system("rm -f output20.txt");
+      return 0;
    }
    fgets(count1, sizeof(count1), fp);
    ret1 = strcmp(count1, setting);
-   if (ret1 == 10)
-      printf("[U-20] Anonymous FTP 비활성화(상) : 양호\n");
-   else
-      printf("[U-20] Anonymous FTP 비활성화(상) : 취약\n");
-
    system("rm -f output20.txt");
+   if (ret1 == 10){
+      printf("[U-20] Anonymous FTP 비활성화(상) : 양호\n");
+      return 1;
+   }
+   else{
+      printf("[U-20] Anonymous FTP 비활성화(상) : 취약\n");
+      return 2;
+   }
 }
